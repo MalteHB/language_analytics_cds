769c71076787e0b13d4dd6a7f6618631c89c49a9
hjb <hjb@kmd.dk>
2021-02-12 15:37:21 +0100

    utils update

--- src/collocation.py
+++ src/collocation.py
@@ -9,6 +9,8 @@ import argparse
 import pandas as pd
 import re
 
+
+
 def main(args):
 
     print("Initiating some awesome collocation!")
@@ -57,94 +59,13 @@ class Collocation:
 
             tokenized_text = self.tokenize(text)
 
-            keyword_occurences = self.get_word_occurence(keyword, tokenized_text)
+
 
             print("Done")
 
             
 
-    def setting_default_data_dir(self):
-        """Setting a default data directory
-
-        Returns:
-            PosixPath: Data directory
-        """
-
-        root_dir = Path.cwd()  # Setting root directory.
-
-        data_dir = root_dir / "data" / "cenlab" / "texts"  # Setting data directory.
-
-        return data_dir
-
-    def setting_default_out_dir(self):
-        """Setting a default Output directory
-
-        Returns:
-            PosixPath: Output directory
-        """
-        root_dir = Path.cwd()  # Setting root directory.
-
-        data_dir = root_dir / "out"  # Setting data directory.
-
-        return data_dir
-
-    def get_filepaths_from_data_dir(self, data_dir, file_extension="*.txt"):
-        """Creates a list containing paths to filenames in a data directoryl
-
-        Args:
-            data_dir (PosixPath): PosixPath to the data directory.
-            file_extension (str): A string with the given file extension you want to extract.
-        """
-
-        files = [file for file in data_dir.glob(file_extension) if file.is_file()]  # Using list comprehension to get all the file names if they are files.
-
-        return files
-
-    def get_filename(self, file):
-        """Creates a list of filenames in a directory.
-
-        Args:
-            files (list): List of file paths
-
-        Returns:
-            filename: list of filenames
-        """
-
-        filename = os.path.split(file)[-1]  # I take the last snippet of the path which is the file and the file extension.
-
-        return filename
-
-    def load_text(self, file):
-        """Loads an image.
-
-        Args:
-            file (PosixPath): A path to an image file.
-
-        Returns:
-            numpy.ndarray: NumPy Array containg all the pixels for the image.
-        """
-
-        # Read each file.
-
-        with open(file, encoding="utf-8") as f:
-
-            text = f.read()
-
-            f.close()
-
-        return text
-
-   
-    def tokenize(self, input_string):
-        
-        # Split on any non-alphanumeric character
-        tokenizer = re.compile(r"\W+")
-        
-        # Tokenize
-        token_list = tokenizer.split(input_string)
-        
-        # Return token_list
-        return token_list
+    
 
     def get_word_occurence(self, keyword, tokenized_text):
         """Gets the number of occurences of a word
---
+++ utils/utils.py
@@ -0,0 +1,87 @@
+#!usr/bin/env python3
+
+from pathlib import Path
+import re
+
+def setting_default_data_dir(self):
+    """Setting a default data directory
+
+    Returns:
+        PosixPath: Data directory
+    """
+
+    root_dir = Path.cwd()  # Setting root directory.
+
+    data_dir = root_dir / "data" / "cenlab" / "texts"  # Setting data directory.
+
+    return data_dir
+
+def setting_default_out_dir(self):
+    """Setting a default Output directory
+
+    Returns:
+        PosixPath: Output directory
+    """
+    root_dir = Path.cwd()  # Setting root directory.
+
+    data_dir = root_dir / "out"  # Setting data directory.
+
+    return data_dir
+
+def get_filepaths_from_data_dir(self, data_dir, file_extension="*.txt"):
+    """Creates a list containing paths to filenames in a data directoryl
+
+    Args:
+        data_dir (PosixPath): PosixPath to the data directory.
+        file_extension (str): A string with the given file extension you want to extract.
+    """
+
+    files = [file for file in data_dir.glob(file_extension) if file.is_file()]  # Using list comprehension to get all the file names if they are files.
+
+    return files
+
+def get_filename(self, file):
+    """Creates a list of filenames in a directory.
+
+    Args:
+        files (list): List of file paths
+
+    Returns:
+        filename: list of filenames
+    """
+
+    filename = file.name  # I take the last snippet of the path which is the file and the file extension.
+
+    return filename
+
+def load_text(self, file):
+    """Loads an image.
+
+    Args:
+        file (PosixPath): A path to an image file.
+
+    Returns:
+        numpy.ndarray: NumPy Array containg all the pixels for the image.
+    """
+
+    # Read each file.
+
+    with open(file, encoding="utf-8") as f:
+
+        text = f.read()
+
+        f.close()
+
+    return text
+
+
+def tokenize(self, input_string):
+    
+    # Split on any non-alphanumeric character
+    tokenizer = re.compile(r"\W+")
+    
+    # Tokenize
+    token_list = tokenizer.split(input_string)
+    
+    # Return token_list
+    return token_list
