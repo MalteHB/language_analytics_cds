2c93845d6c18706d28d39ea49a2d891f09aaf074
MalteHB <malte@hoejmark-bertelsen.dk>
2021-02-13 13:39:49 +0100

    Takes the whole corpus if number_of_files is not specified

--- src/collocation.py
+++ src/collocation.py
@@ -14,101 +14,122 @@ from utils.utils import setting_default_data_dir, setting_default_out_dir, get_f
 
 def main(args):
 
-    print("Initiating some awesome collocation!")
+    print("Word collocation is tough...")
 
     # Importing arguments from the arguments parser
 
-    data_dir = args.data_dir
+    data_dir = args.dd
 
-    out_dir = args.out_dir
+    out_dir = args.od
 
-    Collocation(data_dir=data_dir, out_dir=out_dir)
+    keyword = args.kw
+
+    window_size = args.ws
+
+    number_of_files = args.number_of_files
+
+    Collocation(data_dir=data_dir, out_dir=out_dir, keyword=keyword, window_size=window_size, number_of_files=number_of_files)
 
     print("DONE! Have a nice day. :-)")
 
 
-
 class Collocation:
 
-    def __init__(self, data_dir=None, out_dir=None, keyword="world", window_size=2, number_of_files=10):
+    def __init__(self, data_dir=None, out_dir=None, keyword=None, window_size=None, number_of_files=None):
 
         self.data_dir = data_dir
-
-        self.out_dir = out_dir
-
-        self.keyword = keyword
-
-        self.window_size = window_size
-
-        self.number_of_files = number_of_files
 
         if self.data_dir is None:
 
             self.data_dir = setting_default_data_dir()  # Setting default data directory.
 
+            print(f"\nData directory is not specified.\nSetting it to '{self.data_dir}'.")
+
+        self.out_dir = out_dir
+        
         if self.out_dir is None:
 
             self.out_dir = setting_default_out_dir()  # Setting default output directory.
 
+            print(f"\nOutput directory is not specified.\nSetting it to '{self.out_dir}'.")
+
         self.out_dir.mkdir(parents=True, exist_ok=True)  # Making sure output directory exists.
+
+        self.keyword = keyword
+
+        if self.keyword is None:
+
+            self.keyword = "world"  # Setting default keyword.
+
+            print(f"\nKeyword is not specified.\nSetting it to '{self.keyword}'.")
+
+        self.window_size = window_size
+
+        if self.window_size is None:
+
+            self.window_size = 2  # Setting default window size.
+
+            print(f"\nWindow size is not specified.\nSetting it to '{self.window_size}'.")
 
         files = get_filepaths_from_data_dir(self.data_dir)  # Getting all the absolute filepaths from the data directory.
 
+        self.number_of_files = number_of_files
+
+        if self.number_of_files is None:
+
+            self.number_of_files = len(files)  # Setting number of files to the entire corpus if None.
+
+            print(f"\nNumber of files is not specified.\nSetting it to '{self.number_of_files}'.\nIt is heavily advised to lower it by using the '--number_of_files' argument. Consider using 5.\n\n")
+
         print(f"Initiating collocation calculations for the files in '{self.data_dir}'")
 
-        for file in tqdm(files[0:self.number_of_files]):  # tqdm for progress bar and limiting the amount of files, because these calculations take alot of time.
+        tokenized_text = self.get_concatenated_texts(files[0:self.number_of_files])
 
-            tokenized_text = self.get_tokenized_text(file)
+        collocates = self.word_collocates(tokenized_text=tokenized_text, keyword=self.keyword, window_size=self.window_size)
 
-            collocates = self.word_collocates(tokenized_text=tokenized_text, keyword=keyword, window_size=window_size)
+        raw_frequencies = []
 
-            raw_frequencies = []
+        MIs = []
 
-            MIs = []
+        for collocate in tqdm(collocates):  # tqdm for progress bar
 
-            # print(f"Calculating collocates for '{file.name}'.")
+            raw_frequency = self.raw_frequency(tokenized_text=tokenized_text, keyword=collocate)  # Raw frequency of collocate
 
-            for collocate in tqdm(collocates):  # tqdm for progress bar
+            O11 = self.joint_frequency(tokenized_text=tokenized_text, keyword=self.keyword, collocate=collocate, window_size=self.window_size)  # Joint frequency of keyword and collocate
 
-                raw_frequency = self.raw_frequency(tokenized_text=tokenized_text, keyword=collocate)  # Raw frequency of collocate
+            O12 = self.disjoint_frequency(tokenized_text=tokenized_text, keyword=self.keyword, collocate=collocate, window_size=self.window_size)  # Disjoint frequency of keyword and collocate
 
-                O11 = self.joint_frequency(tokenized_text=tokenized_text, keyword=keyword, collocate=collocate, window_size=window_size)  # Joint frequency of keyword and collocate
+            O21 = self.disjoint_frequency(tokenized_text=tokenized_text, keyword=collocate, collocate=self.keyword, window_size=self.window_size)  # Disjoint frequency of collocate as keyword and keyword as collocate
 
-                O12 = self.disjoint_frequency(tokenized_text=tokenized_text, keyword=keyword, collocate=collocate, window_size=window_size)  # Disjoint frequency of keyword and collocate
+            O22 = self.n_words_without_keyword_and_collocate(tokenized_text=tokenized_text, keyword=self.keyword, collocate=collocate)  # All the words in the corpus that are not either the keyword nor the collocate
 
-                O21 = self.disjoint_frequency(tokenized_text=tokenized_text, keyword=collocate, collocate=keyword, window_size=window_size)  # Disjoint frequency of collocate as keyword and keyword as collocate
+            N = O11 + O12 + O21 + O22  # There is doubt whether to calculate N like this or by simply taking len(tokenized_text)
 
-                O22 = self.n_words_without_keyword_and_collocate(tokenized_text=tokenized_text, keyword=keyword, collocate=collocate)  # All the words in the corpus that are not either the keyword nor the collocate
+            R1 = O11 + O12  # Calculating R1
 
-                N = O11 + O12 + O21 + O22  # There is doubt whether to calculate N like this or by simply taking len(tokenized_text)
+            C1 = O11 + O21  # Calculating C1
 
-                R1 = O11 + O12  # Calculating R1
+            # Expected
+            E11 = (R1 * C1 / N)  # Expected frequency
 
-                C1 = O11 + O21  # Calculating C1
+            # return MI
+            MI = np.log2(O11 / E11)  # Mutual information
 
-                # Expected
-                E11 = (R1 * C1 / N)  # Expected frequency
+            raw_frequencies.append(raw_frequency)
 
-                # return MI
-                MI = np.log2(O11 / E11)  # Mutual information
+            MIs.append(MI)
 
-                raw_frequencies.append(raw_frequency)
+        data_dict = {"collocate": collocates,
+                     "raw_frequency": raw_frequencies,
+                     "MI": MIs}
 
-                MIs.append(MI)
+        df = pd.DataFrame(data=data_dict)
 
-            data_dict = {"collocate": collocates,
-                         "raw_frequency": raw_frequencies,
-                         "MI": MIs}
+        df = df.sort_values("raw_frequency", ascending=False)  # Sorting collocates with highest frequency at the top.
 
-            df = pd.DataFrame(data=data_dict)
+        write_path = self.out_dir / "collocates.csv"
 
-            df = df.sort_values("raw_frequency", ascending=False)  # Sorting collocates with highest frequency at the top.
-
-            write_path = self.out_dir / f"collocates_{file.stem}.csv"
-
-            df.to_csv(write_path)
-
-        print("Done")
+        df.to_csv(write_path)
 
 
     def get_tokenized_text(self, file):
@@ -240,16 +261,34 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--data_dir',
-                        metavar="data_dir",
+    parser.add_argument('--dd',
+                        metavar="Data Directory",
                         type=str,
-                        help='A PosixPath to the data directory',
+                        help='A PosixPath to the data directory.',
                         required=False)
 
-    parser.add_argument('--out_dir',
-                        metavar="out_dir",
+    parser.add_argument('--od',
+                        metavar="Output Directory",
                         type=str,
-                        help='A path to the output directory',
+                        help='A path to the output directory.',
+                        required=False)
+
+    parser.add_argument('--kw',
+                        metavar="Keyword",
+                        type=str,
+                        help='The keyword to find collocates for.',
+                        required=False)
+
+    parser.add_argument('--ws',
+                        metavar="Window Size",
+                        type=int,
+                        help='The given window size.',
+                        required=False)
+
+    parser.add_argument('--number_of_files',
+                        metavar="Number of Files",
+                        type=int,
+                        help='Number of files to create collocates from in the data directory.',
                         required=False)
 
     main(parser.parse_args())
